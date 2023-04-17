5d99443466596c8e53698ba4ebca9c5f2d4f9001
nascimentodorafael <107429969+nascimentodorafael@users.noreply.github.com>
2023-04-17 15:10:08 -0300

    Add files via upload

---
+++ TimeLine/js/idade.js
@@ -0,0 +1,8 @@
+function carregaJovem() {
+    let nome = document.getElementById('nome').value;
+    let idade = document.getElementById('idade').value;
+
+    let IdadeFutura = idade + 40;
+
+    return document.getElementById('IdadeFutura').value = IdadeFutura;
+} 
